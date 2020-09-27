d2af375755bdcabf33502cf044e454ee95a5e9a8
virus927 <64857295+virus927@users.noreply.github.com>
2020-09-27 20:17:48 +0300

    Update lab5.java

--- src/ru/mirea/lab5.java
+++ src/ru/mirea/lab5.java
@@ -2,12 +2,17 @@ package ru.mirea;
 
 public class lab5 {
         public static void main(String args[]) {
-
             String palindrom = ("deed");
             StringBuffer buffer = new StringBuffer(palindrom);
             buffer.reverse();
             String palindrom2 = new String(buffer);
-            System.out.println(palindrom.equals(palindrom2));
+            if((palindrom.equals(palindrom2)==true))
+            {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
         }
         //virus927
     }
